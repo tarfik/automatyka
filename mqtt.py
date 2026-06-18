8dfb0c525744790b74a79b2754ce32f5e1056ef8
Dominik Mostowiec <dominikmostowiec@gmail.com>
2026-06-18 23:25:23 +0200

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -1,26 +1,30 @@
 from umqtt.simple import MQTTClient
 import time
 
-MQTT_BROKER = "YOUR_HIVEMQ_HOST"
-CLIENT_ID = "drzwi-node"
 
 TOPIC_CMD = b"drzwi/cmd"
 TOPIC_STATUS = b"drzwi/status"
 TOPIC_LOG = b"drzwi/log"
 
+def get_time_pl():
+    t = time.localtime(time.time() + 3600)
+    return "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(
+        t[0], t[1], t[2], t[3], t[4], t[5]
+    )
+
 class MQTT:
-    def __init__(self, motor, ota):
+    def __init__(self, motor, ota, mqtt_client_id, mqtt_host, mqtt_port, mqtt_user, mqtt_password):
         self.motor = motor
         self.ota = ota
 
         self.client = MQTTClient(
-            CLIENT_ID,
-            MQTT_BROKER,
-            user="USER",
-            password="PASS",
+            mqtt_client_id,
+            mqtt_host,
+            mqtt_port,
+            user=mqtt_user,
+            password=mqtt_password,
             keepalive=60
         )
-
         self.client.set_callback(self.callback)
 
     def connect(self):
@@ -35,26 +39,30 @@ class MQTT:
         self.client.publish(TOPIC_LOG, msg)
 
     def callback(self, topic, msg):
-        msg = msg.decode()
+        try:
+            msg = msg.decode()
+            self.log("On {} received command: {}".format(get_time_pl(), msg))
 
-        if msg == "OPEN":
-            self.publish("OPENING")
-            self.motor.open()
-            time.sleep(10)
-            self.motor.stop()
-            self.publish("OPEN")
+            if msg == "OPEN":
+                self.publish("OTWIERANIE")
+                self.motor.open()
+                time.sleep(10)
+                self.motor.stop()
+                self.publish("OTWARTE")
 
-        elif msg == "CLOSE":
-            self.publish("CLOSING")
-            self.motor.close()
-            time.sleep(10)
-            self.motor.stop()
-            self.publish("CLOSED")
+            elif msg == "CLOSE":
+                self.publish("ZAMYKANIE")
+                self.motor.close()
+                time.sleep(10)
+                self.motor.stop()
+                self.publish("ZAMKNIĘTE")
 
-        elif msg == "STOP":
-            self.motor.stop()
-            self.publish("STOPPED")
+            elif msg == "STOP":
+                self.motor.stop()
+                self.publish("ZATRZYMANE")
 
-        elif msg == "UPDATE":
-            self.publish("UPDATING")
-            self.ota.update()
+            elif msg == "UPDATE":
+                self.publish("AKTUALIZACJA")
+                self.ota.update()
+        except Exception as e:
+            self.log("Error in callback: {}".format(e))
