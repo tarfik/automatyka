81e53af62e7ee1af5d6f0cdc8c89b73158cfaf2e
Dominik Mostowiec <dominikmostowiec@gmail.com>
2026-06-18 23:24:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 from motor import Motor
 from mqtt import MQTT
 from ota import OTA
-from conf import wifi_ssid, wifi_password
+from conf import wifi_ssid, wifi_password, mqtt_client_id, mqtt_host, mqtt_port, mqtt_user, mqtt_password
+import ntptime
 
 def wifi_connect():
     wlan = network.WLAN(network.STA_IF)
@@ -12,15 +13,21 @@ def wifi_connect():
     wlan.connect(wifi_ssid, wifi_password)
 
     while not wlan.isconnected():
+        print('waiting for connection')
         time.sleep(1)
 
-motor = Motor()
-ota = OTA()
-mqtt = MQTT(motor, ota)
+try:
+    ntptime.settime()
+    motor = Motor()
+    ota = OTA()
+    mqtt = MQTT(motor, ota, mqtt_client_id, mqtt_host, mqtt_port, mqtt_user, mqtt_password)
 
-wifi_connect()
-mqtt.connect()
+    wifi_connect()
+    mqtt.connect()
 
-while True:
-    mqtt.client.check_msg()
-    time.sleep(0.1)
+    while True:
+        mqtt.client.check_msg()
+        time.sleep(0.5)
+except Exception as e:
+    print("Error:", e)
+    raise
