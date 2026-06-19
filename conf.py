040004fd0be8b36e72b67dec36a2579e6973deee
Dominik Mostowiec <dominikmostowiec@gmail.com>
2026-06-19 09:11:22 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,2 +1,8 @@
-wifi_ssid = ""
-wifi_password = ""
+wifi_ssid = "xx"
+wifi_password = "xx"
+mqtt_host = "xx"
+mqtt_port = 1
+mqtt_client_id = ""
+mqtt_user = ''
+mqtt_password = ''
+motor_emergency_timeout_s = 25
